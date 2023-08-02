309b6f47bd372771dc3e32aa9ec1d3786ced5ba7
Harshit Verma <harshitvermaofficial59@gmail.com>
2023-08-02 13:57:25 +0530

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import css from "./App.css";
 import Menu from './Menu';
-import Search from './Search';
+import Search from './Search.jsx';
 
 function App() {
     return (
